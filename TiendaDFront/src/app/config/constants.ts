1dd44b457a6b2bf8016506b2aaa49d71ddb67207
Jon Campaña <48183794+jon2n1011@users.noreply.github.com>
2022-06-07 16:02:13 +0200

    Comunicaciones con API

---
+++ TiendaDFront/src/app/config/constants.ts
@@ -0,0 +1,8 @@
+// Angular Modules
+import { Injectable } from '@angular/core'; 
+@Injectable() 
+export class Constants {
+public readonly API_ENDPOINT: string = 'https://localhost:44383/api/'; 
+//public readonly API_MOCK_ENDPOINT: string = 'https://www.userdomainmock.com/'; 
+public static TitleOfSite: string = " Tienda DropShipping"; 
+} 
---
+++ TiendaDFront/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ConfigService } from '../config/config.service';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Constants } from '../config/constants';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+  listaAlgo: string[]=[];
+  constructor(private http:HttpClient) {
+   //Aqui es lo que se ejecut al principio
+  let x=this.http.get<ConfigService>("https://localhost:44383/api/ordenes",{observe:'response',responseType:'json'});
+  }
+
+  public login() {
+
+   
+     //this.listaAlgo=this.http.get<any>("https://localhost:44383/api/ordenes",{observe:'response',responseType:'json'});
+
+  }
+  ngOnInit(): void {
+    this.http.get("https://localhost:44383/api/ordenes").subscribe(values => {
+      this.listaAlgo = values as string[];
+  });
+  }
+}
